7223c80c562dc426804c5446a0e67e7c576d75a2
Jorlost19 <jorlost19@gmail.com>
2019-06-12 19:39:49 -0500

    finished Register View

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Register from './Register';
+import {StylesProvider, createGenerateClassName} from '@material-ui/styles';
 
 function App() {
   return (
-    <h1>Launch Partner</h1>
+    <StylesProvider generateClassName={createGenerateClassName()}>
+        <h1>Launch Partner</h1>
+        <Route path='/' exact component={Register} />
+    </StylesProvider>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { BrowserRouter as Router } from'react-router-dom';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import theme from './helpers/theme';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router>
+                    <MuiThemeProvider theme={theme}>
+                        <App /> 
+                    </MuiThemeProvider>
+                </Router>, document.getElementById('root'));
